40b6af685a2e69f47097ed068af0abbd81502b15
Braden Hitchcock <hitchcock.braden@gmail.com>
2018-06-05 12:49:04 -0700

    initializing library structure

---
+++ projects/contextmenu/src/lib/contextmenu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngux-contextmenu',
+  template: `
+    <p>
+      contextmenu works!
+    </p>
+  `,
+  styles: []
+})
+export class ContextmenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/contextmenu/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of contextmenu
+ */
+
+export * from './lib/contextmenu.service';
+export * from './lib/contextmenu.component';
+export * from './lib/contextmenu.module';
